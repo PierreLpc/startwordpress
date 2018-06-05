c9ed33d7337ab25df9cdea1701c906ef68989958
Pierre <pierre.lepicier@viacesi.fr>
2018-06-05 14:58:21 +0200

    modife col-8 a col-12

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div class="row">
-		<div class="col-sm-8 blog-main">
+		<div class="col-sm-12 blog-main">
 			<?php 
 			if ( have_posts() ) : while ( have_posts() ) : the_post();
 				get_template_part( 'content', get_post_format() );
